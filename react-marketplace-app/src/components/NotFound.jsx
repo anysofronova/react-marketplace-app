b5c93bb3add89b7755c9c780e9b233a432c75b24
Anna <anysofronova@yandex.ru>
2022-03-23 11:08:16 +0300

    NotFound Page

--- react-marketplace-app/src/components/NotFound.jsx
+++ react-marketplace-app/src/components/NotFound.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./scss/NotFound.scss";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>
-      <h2>The page is not found</h2>
+    <div className="notFound">
+      <div class="notFound__face">
+        <div class="notFound__band">
+          <div class="notFound__red"></div>
+          <div class="notFound__white"></div>
+          <div class="notFound__blue"></div>
+        </div>
+        <div class="notFound__eyes"></div>
+        <div class="notFound__dimples"></div>
+        <div class="notFound__mouth"></div>
+      </div>
+
+      <h1 className="notFound__title">Oops! Something went wrong!</h1>
+      <Link to={"/"}>
+        <div class="notFound__btn">Return to Home</div>
+      </Link>
     </div>
   );
 }
