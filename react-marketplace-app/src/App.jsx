f5b4b3e8bf23077a9d6b07b547da22f38962d62a
Anna <anysofronova@yandex.ru>
2022-03-23 10:24:11 +0300

    Configure React app for deployment to GitHub Pages

--- react-marketplace-app/src/App.jsx
+++ react-marketplace-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Landing />} />
           <Route path="/store/:storeID" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
