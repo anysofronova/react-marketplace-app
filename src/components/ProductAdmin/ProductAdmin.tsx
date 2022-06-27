7a97630bbb0f575b7db6fcd71e31da0d5f3455e6
Anna <anysofronova@yandex.ru>
2022-06-27 21:17:38 +0300

    Bug: Changing the size of the browser window

--- src/components/ProductAdmin/ProductAdmin.tsx
+++ src/components/ProductAdmin/ProductAdmin.tsx
@@ -2,10 +2,20 @@ import AddProductForm from "./AddProductForm/AddProductForm";
 import styles from "./ProductAdmin.module.scss";
 import { useAppSelector } from "../../hooks/redux";
 import EditProductForm from "./EditProductForm/EditProductForm";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useWidth from "../../hooks/useWidth";
 
 const ProductAdmin: FC = () => {
   const products = useAppSelector((state) => state.mainSlice.productList);
+  const navigate = useNavigate();
+  const width = useWidth();
+  useEffect(() => {
+    if (width > 900) {
+      navigate("/");
+    }
+  }, [width, navigate]);
+
   return (
     <header className={styles.productAdmin}>
       <div className={styles.wrap}>
